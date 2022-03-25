5eaecc0ea39173c0390eb37d0ee9e4f0cdf0f2ed
RafaelFurlanetto <rafael.furlanetto@uel.br>
2022-03-25 18:39:44 -0300

    Implementando parte do RPC

--- ExercicioRPC/server.c
+++ ExercicioRPC/server.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <string.h>
+#include "doublyLinkedList.h"
 #include "notebook.h"
 
 /**
@@ -9,30 +11,107 @@
  * - Criação da lista de pessoas
  */
 
-struct contact *create_1_svc(contact *argp, struct svc_req *rqstp)
+/*
+    Implementação da Interface de comunicação RPC do servidor
+*/
+contact *create_1_svc(contact *argp, struct svc_req *rqstp)
 {
+
     static contact result;
 
+    printf("[INFO] Criando novo contato.\n[INFO] Nome: %s\n[INFO] Número: %s\n[INFO] Endereço: %s\n\n", argp->name, argp->number, argp->address);
+    strcpy(result.name, argp->name);
+    strcpy(result.number, argp->number);
+    strcpy(result.address, argp->address);
+
+    DoublyLinkedList lista = createDoublyLinkedList();
+    loadList(lista);
+    /*
+        TODO: Adicionar na lista
+    */
+    saveList(lista);
+    removeList(lista, 0);
     return (&result);
 }
 
-struct contact *read_1_svc(contact *argp, struct svc_req *rqstp)
+contact *read_1_svc(contact *argp, struct svc_req *rqstp)
 {
     static contact result;
 
+    printf("[INFO] Lendo contato.\n");
+
+    DoublyLinkedList lista = createDoublyLinkedList();
+    loadList(lista);
+    /*
+        TODO: Adicionar na lista
+    */
+    saveList(lista);
+    removeList(lista, 0);
     return (&result);
 }
 
-struct contact *update_1_svc(contact *argp, struct svc_req *rqstp)
+contact *update_1_svc(contact *argp, struct svc_req *rqstp)
 {
     static contact result;
 
+    printf("[INFO] Alterando contato.\n");
+
+    DoublyLinkedList lista = createDoublyLinkedList();
+    loadList(lista);
+    /*
+        TODO: Adicionar na lista
+    */
+    saveList(lista);
+    removeList(lista, 0);
     return (&result);
 }
 
-struct contact *delete_1_svc(contact *argp, struct svc_req *rqstp)
+contact *delete_1_svc(contact *argp, struct svc_req *rqstp)
 {
     static contact result;
 
+    printf("[INFO] Removendo contato.\n");
+
+    DoublyLinkedList lista = createDoublyLinkedList();
+    loadList(lista);
+    /*
+        TODO: Adicionar na lista
+    */
+    saveList(lista);
+    removeList(lista, 0);
     return (&result);
+}
+
+
+
+
+
+void saveList(DoublyLinkedList lista)
+{
+    FILE* notebookFilePtr = fopen("notebook.bin", "wb");
+
+    for(Node aux = getFirst(lista); aux != NULL; aux = getNext(aux))
+    {
+        Info info = getInfo(aux);
+        fwrite(&info, sizeof(contact), 1, notebookFilePtr);
+    }
+    fclose(notebookFilePtr);
+}
+
+void loadList(DoublyLinkedList lista)
+{
+    FILE* notebookFilePtr = fopen("notebook.bin", "rb");
+    if(notebookFilePtr == NULL)
+    {
+        return;
+    }
+    
+    contact newContact;
+
+    /* Attempt to read */
+    // while (fread(&newContact, sizeof(struct contact), 1, notebookFilePtr) == sizeof(struct contact)) {
+    //     insert(lista, newContact);
+    // }
+
+    fclose(notebookFilePtr);
 }
