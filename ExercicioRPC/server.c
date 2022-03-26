28ae9dcb16ab01e0418c0c5047f6849ff6bf1e80
RafaelFurlanetto <rafael.furlanetto@uel.br>
2022-03-26 08:45:58 -0300

    Adicionando Makefile

--- ExercicioRPC/server.c
+++ ExercicioRPC/server.c
@@ -3,6 +3,37 @@
 #include "doublyLinkedList.h"
 #include "notebook.h"
 
+void saveList(DoublyLinkedList lista)
+{
+    FILE* notebookFilePtr = fopen("notebook.bin", "wb");
+
+    for(Node aux = getFirst(lista); aux != NULL; aux = getNext(aux))
+    {
+        Info info = getInfo(aux);
+        fwrite(&info, sizeof(contact), 1, notebookFilePtr);
+    }
+    fclose(notebookFilePtr);
+}
+
+void loadList(DoublyLinkedList lista)
+{
+    FILE* notebookFilePtr = fopen("notebook.bin", "rb");
+    if(notebookFilePtr == NULL)
+    {
+        return;
+    }
+    
+    contact newContact;
+
+    /* Attempt to read */
+    // while (fread(&newContact, sizeof(struct contact), 1, notebookFilePtr) == sizeof(struct contact)) {
+    //     insert(lista, newContact);
+    // }
+
+    fclose(notebookFilePtr);
+}
+
+
 /**
  * Implementação das funções definidas no notebook.h criadas pelo rpcgen
  * Funções básicas de CRUD
@@ -86,32 +117,3 @@ contact *delete_1_svc(contact *argp, struct svc_req *rqstp)
 
 
 
-void saveList(DoublyLinkedList lista)
-{
-    FILE* notebookFilePtr = fopen("notebook.bin", "wb");
-
-    for(Node aux = getFirst(lista); aux != NULL; aux = getNext(aux))
-    {
-        Info info = getInfo(aux);
-        fwrite(&info, sizeof(contact), 1, notebookFilePtr);
-    }
-    fclose(notebookFilePtr);
-}
-
-void loadList(DoublyLinkedList lista)
-{
-    FILE* notebookFilePtr = fopen("notebook.bin", "rb");
-    if(notebookFilePtr == NULL)
-    {
-        return;
-    }
-    
-    contact newContact;
-
-    /* Attempt to read */
-    // while (fread(&newContact, sizeof(struct contact), 1, notebookFilePtr) == sizeof(struct contact)) {
-    //     insert(lista, newContact);
-    // }
-
-    fclose(notebookFilePtr);
-}
