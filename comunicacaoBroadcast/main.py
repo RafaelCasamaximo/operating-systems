4b7cdf1d4ec132c5086cb25fb74947d0ed7e53e1
RafaelFurlanetto <rafael.furlanetto@uel.br>
2022-05-29 23:57:58 -0300

    Adicionando Click library e logs

--- comunicacaoBroadcast/main.py
+++ comunicacaoBroadcast/main.py
@@ -1,36 +1,48 @@
+from email.policy import default
 from chat import Chat
-import sys
+import click
+from termcolor import colored
 
-def main():
-    size = len(sys.argv)
+@click.command()
+@click.option('--userName', '-u', required=True, help='Nome de usuário no chat.')
+@click.option('--port', '-p', default=55555, help='Porta que será utilizada na execução. [DEFAULT=55555]')
+@click.option('--start', '-s', is_flag=True, help='Flag indicando que a execução iniciará um novo chat.')
+@click.option('--enter', '-e', is_flag=True, help='Flag indicando que a execução entrará em um chat já existente.')
+@click.option('--hostIp', '-hip', help='IP do host para a conexão. Para essa opção ser válida o programa deverá possuir a flag --enter/ -e.')
+@click.option('--hostPort', '-hp', help='Porta do host para a conexão. Para essa opção ser válida o programa deverá possuir a flag --enter/ -e.')
+def cli(username, port, start, enter, hostip, hostport):
+    # ERROR CHECKING
+    if start == False and enter == False:
+        print(colored("[ERROR] ", "red"), "É necessário ou criar um chat ou entrar em um chat para a execução do programa. Execute com a flag --help para ver as opções.")
+        return
 
-    ''' 
-        Flags:
-            -p: Porta da nova ponta (obrigatório)
-            -n: Criar novo chat
-            -u: Usar chat. Passar ip e porta do host
+    if start == True and enter == True:
+        print(colored("[ERROR] ", "red"), "Não é possível criar e entrar em um chat ao mesmo tempo. Execute com a flag --help para ver as opções.")
+        return
 
-        Exemplos:
-            $ python3 ./main.py -p 44404 Iury -n (Cria um novo chat na porta 44404)
-            $ python3 ./main.py -p 44405 Rafael -u 127.0.1.1 44404 (Conecta com o host 127.0.1.1:44404)
-    '''
+    if enter == True and (hostip == None or hostport == None):
+        print(colored("[ERROR] ", "red"), "Para entrar em um chat é necessário informar o IP e a porta do host. Execute com a flag --help para ver as opções.")
+        return
+    if port == 55555:
+        print(colored("[WARNING] ", "yellow"), "Utilizando a porta padrão", colored("55555.", "yellow"), "Essa porta pode estar sendo utilizada por outra execução ou processo. É recomendado alterar.")   
 
-    if size > 1:
-        for i in range(1, len(sys.argv)):
-            if sys.argv[i] == '-p':
-                chat = Chat(int(sys.argv[i + 1]), sys.argv[i + 2])
 
-            elif sys.argv[i] == '-n':
-                chat.createChat()
+    # INSTANCIANDO CHAT
+    print(colored("[INFO] ", "blue"), "Instanciando novo chat...")
+    chat = Chat(int(port), username)
+    print(colored("[INFO] ", "blue"), "Chat instanciado com sucesso.")
 
-            elif sys.argv[i] == '-u':
-                chat.useChat(sys.argv[i + 1], int(sys.argv[i + 2]))
-            
-            else:
-                continue
-
+    if start == True:
+        print(colored("[INFO] ", "blue"), "Criando nova sala...")
+        chat.createChat()
+        print(colored("[INFO] ", "blue"), "Sala criada com sucesso. Já é possível enviar mensagens.")
+    elif enter == True:
+        print(colored("[INFO] ", "blue"), "Entrando na sala...")
+        chat.useChat(hostip, int(hostport))
+        print(colored("[INFO] ", "blue"), "Conexão realizada com sucesso. Já é possível enviar mensagens.")
     else:
-        print("Leia README para executar o programa corretamente.")
+        print(colored("[ERROR] ", "red"), "Ocorreu algum erro nos argumentos. Verifique os dados e tente novamente.")
+    pass
 
 if __name__ == "__main__":
-    main()
+    cli()
